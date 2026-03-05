3494ccdff7eea1e492b7173742b53f598475c6a7
ddarahakit2026 <ddarahakit2026@gmail.com>
2026-03-05 15:51:43 +0900

    [Feat] 카프카로 데이터 동기화
    이벤츠 기반으로 동작하도록 EDA 적용

--- board_command/src/main/java/com/example/board_command/api/BoardService.java
+++ board_command/src/main/java/com/example/board_command/api/BoardService.java
@@ -2,17 +2,22 @@ package com.example.board_command.api;
 
 import com.example.board_command.api.model.Board;
 import com.example.board_command.api.model.BoardDto;
+import com.example.board_command.api.model.BoardEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class BoardService {
     private final BoardRepository boardRepository;
+    private final KafkaTemplate<Long, BoardEvent.Create> kafkaTemplate;
 
     public BoardDto.RegRes register(Long userIdx, String userName, BoardDto.RegReq dto) {
-
         Board entity = boardRepository.save(dto.toEntity(userIdx, userName));
+
+        // 카프카에 게시글이 생성됐다는 메시지(게시글 내용)를 전송
+        kafkaTemplate.send("board", entity.getIdx(), BoardEvent.Create.from(entity));
 
         return BoardDto.RegRes.from(entity);
     }
